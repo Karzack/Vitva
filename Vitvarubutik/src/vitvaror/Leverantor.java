2780e142b0940f9a4c1100257b2bbff1c183d636
Ludwigninn <ludwigninn@hotmail.com>
2016-05-18 10:52:44 +0200

    Leverantor

--- Vitvarubutik/src/vitvaror/Leverantor.java
+++ Vitvarubutik/src/vitvaror/Leverantor.java
@@ -1,5 +1,39 @@
 package vitvaror;
 
 public class Leverantor {
-
+	private int levid;
+	private String namn;
+	private int telefonr;
+	private String adress;
+	public Leverantor(int levid, String namn, int telefonr, String adress) {
+		
+		this.levid = levid;
+		this.namn = namn;
+		this.telefonr = telefonr;
+		this.adress = adress;
+	}
+	public int getLevid() {
+		return levid;
+	}
+	public void setLevid(int levid) {
+		this.levid = levid;
+	}
+	public String getNamn() {
+		return namn;
+	}
+	public void setNamn(String namn) {
+		this.namn = namn;
+	}
+	public int getTelefonr() {
+		return telefonr;
+	}
+	public void setTelefonr(int telefonr) {
+		this.telefonr = telefonr;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
 }
