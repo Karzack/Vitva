f1dd3dd4a37463978bb143bd9e6502157f4d1d4e
Karzack <bj88svensson@gmail.com>
2016-05-18 11:58:28 +0200

    Dbhandler update
    kop

--- Vitvarubutik/src/vitvaror/DatabaseHandler.java
+++ Vitvarubutik/src/vitvaror/DatabaseHandler.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class DatabaseHandler {
 	private static final String DATABASE_USER = "AF2015";
 	private static final String DATABASE_PASS = "mongo";
-	
+
 	private Connection connection;
 
 	public DatabaseHandler() {
@@ -25,60 +25,76 @@ public class DatabaseHandler {
 	public int getCustomerIdByName(String namn) throws SQLException {
 		PreparedStatement query = connection.prepareStatement("SELECT kundid FROM kund WHERE namn=?");
 		query.setString(1, namn);
-        ResultSet rs = query.executeQuery();
-        rs.next();
-        return rs.getInt(1);
+		ResultSet rs = query.executeQuery();
+		rs.next();
+		return rs.getInt(1);
 	}
-	
+
 	public void updateCustomer(int kundid, String namn, String email, String telefonnr, String adress) throws SQLException {
 		PreparedStatement query = connection.prepareStatement( "UPDATE kund SET namn=?, email=?, telefonnr=?, adress=? WHERE kundid=?");
-        query.setString(1, namn);
-        query.setString(2, email);
-        query.setString(3, telefonnr);
-        query.setString(4, adress);
-        query.setInt(5, kundid);
-        query.executeUpdate();
+		query.setString(1, namn);
+		query.setString(2, email);
+		query.setString(3, telefonnr);
+		query.setString(4, adress);
+		query.setInt(5, kundid);
+		query.executeUpdate();
+	}
+
+	public void addCustomer(String namn, String email, String telefonnr, String adress) throws SQLException {
+		PreparedStatement query = connection.prepareStatement( "INSERT INTO kund(namn, email, telefonnr, adress) values (?, ?, ?, ?)");
+		query.setString(1, namn);
+		query.setString(2, email);
+		query.setString(3, telefonnr);
+		query.setString(4, adress);
+		query.executeUpdate();
+	}
+
+	public ArrayList<Kund> getCustomers() throws SQLException {
+		PreparedStatement query = connection.prepareStatement("SELECT * FROM kund");
+		ResultSet rs = query.executeQuery();
+		ArrayList<Kund> resList = new ArrayList<Kund>();
+		while (rs.next()) {
+			Kund row = new Kund(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5));
+			resList.add(row);
+		}
+
+		return resList;
+	}
+
+	public Kund getCustomer(int kundId) throws SQLException {
+		PreparedStatement query = connection.prepareStatement("SELECT * FROM kund where kundid=?");
+		query.setInt(1, kundId);
+		ResultSet rs = query.executeQuery();
+		rs.next();
+		return new Kund(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5));
 	}
 	
-	public void addCustomer(String namn, String email, String telefonnr, String adress) throws SQLException {
-        PreparedStatement query = connection.prepareStatement( "INSERT INTO kund(namn, email, telefonnr, adress) values (?, ?, ?, ?)");
-        query.setString(1, namn);
-        query.setString(2, email);
-        query.setString(3, telefonnr);
-        query.setString(4, adress);
-        query.executeUpdate();
-    }
-	
-	public ArrayList<Kund> getCustomers() throws SQLException {
-        PreparedStatement query = connection.prepareStatement("SELECT * FROM kund");
-        ResultSet rs = query.executeQuery();
-        ArrayList<Kund> resList = new ArrayList<Kund>();
-        while (rs.next()) {
-            Kund row = new Kund(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5));
-            resList.add(row);
-        }
-        
-        return resList;
-    }
+	public Artikel getArticle(int artikelId) throws SQLException{
+		PreparedStatement query = connection.prepareStatement("SELECT * FROM artikel where artikelid = ?");
+		query.setInt(1, artikelId);
+		ResultSet rs = query.executeQuery();
+		rs.next();
+		return new Artikel(rs.getInt(1),rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6),rs.getInt(7),rs.getInt(8),rs.getInt(9));
+	}
 	
 	public void addArticle(String namn, String tillverkare, String modell, String energiklass, String beskrivning, int pris, int inkopspris, int antal) throws SQLException {
-        PreparedStatement query = connection.prepareStatement( "INSERT INTO artikel(namn, tillverkare, modell, energiklass, beskrivning, pris, inköpspris, antal) values (?, ?, ?, ?, ?, ?, ?, ?)");
-        query.setString(1, namn);
-        query.setString(2, tillverkare);
-        query.setString(3, modell);
-        query.setString(4, energiklass);
-        query.setString(5, beskrivning);
-        query.setInt(6, pris);
-        query.setInt(7, inkopspris);
-        query.setInt(8, antal);
-        query.executeUpdate();
-    }
-	
+		PreparedStatement query = connection.prepareStatement( "INSERT INTO artikel(namn, tillverkare, modell, energiklass, beskrivning, pris, inkï¿½pspris, antal) values (?, ?, ?, ?, ?, ?, ?, ?)");
+		query.setString(1, namn);
+		query.setString(2, tillverkare);
+		query.setString(3, modell);
+		query.setString(4, energiklass);
+		query.setString(5, beskrivning);
+		query.setInt(6, pris);
+		query.setInt(7, inkopspris);
+		query.setInt(8, antal);
+		query.executeUpdate();
+	}
+
 	public static void main(String[] args) throws SQLException {
 		DatabaseHandler handler = new DatabaseHandler();
-		//handler.addCustomer("Alexander Johansson", "alexanderjoh94@gmail.com", "0707320788", "Södra Förstadsgatan 132");
+		//handler.addCustomer("Alexander Johansson", "alexanderjoh94@gmail.com", "0707320788", "Sï¿½dra Fï¿½rstadsgatan 132");
 		//handler.addArticle("Logitech G35", "Logitech", "G35", "", "Headset", 1000, 700, 150);
-		
+
 		ArrayList<Kund> customerList = handler.getCustomers();
 		for(Kund customer : customerList) {
 			System.out.println(customer.getNamn() + " " + customer.getKundId());
